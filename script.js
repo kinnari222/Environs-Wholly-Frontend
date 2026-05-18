656ff426a802239c4fdc1c22f5ad5b0b58dcf7c2
KIRA <krk2294@gmail.com>
2026-05-18 15:52:08 +0530

    Email service for contact us page

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
-document.querySelector(".btn-primary").addEventListener("click", function () {
-    alert("Thank you for your interest in environmental conservation!");
-});
+// document.querySelector(".btn-primary").addEventListener("click", function () {
+//     alert("Thank you for your interest in environmental conservation!");
+// });
+
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const data = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      phone: document.getElementById("phone").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    };
+
+    try {
+      const response = await fetch("http://localhost:5000/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      console.log("response---", response)
+
+      const result = await response.json();
+      console.log("result--", result)
+
+      if (result.success) {
+        alert("Message sent successfully!");
+
+        document.getElementById("contactForm").reset();
+      } else {
+        alert("Failed to send message");
+      }
+    } catch (error) {
+      console.log(error);
+
+      alert("Something went wrong");
+    }
+  });
